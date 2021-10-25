b72e9605edc904c5d71edbb1e532a425ef3e3263
dev_julian <forydeveloper@gmail.com>
2021-10-25 14:58:28 -0500

    handle errors

--- screens/Calculator.tsx
+++ screens/Calculator.tsx
@@ -2,23 +2,34 @@ import * as React from "react";
 import { Text, View } from "../components/Themed";
 import styles from "../constants/Styles";
 import ButtonT from "../components/ButtonT";
+import { Alert } from "react-native";
 
 const Calculator = () => {
   const [inputs, setInputs] = React.useState<string>("");
   const cells = [9, 8, 7, "AC", 6, 5, 4, "+", 3, 2, 1, "-", 0, "*", "/", "="];
 
-  const calculate = () => setInputs(new Function("return " + inputs)());
+  const calculate = () => {
+    try {
+      setInputs(new Function("return " + inputs)());
+    } catch (error) {
+      Alert.alert("Operacion invalida");
+    }
+  };
 
   const handleInput = (val: string | number) => {
-    if (typeof val === "number") {
-      setInputs(inputs + val.toString());
+    if (inputs.length >= 24 && val !== "AC") {
+      Alert.alert("Numero de caracteres exedido");
     } else {
-      if (val === "AC") {
-        setInputs("");
-      } else if (val === "=") {
-        calculate();
+      if (typeof val === "number") {
+        setInputs(inputs + val.toString());
       } else {
-        setInputs(inputs + val);
+        if (val === "AC") {
+          setInputs("");
+        } else if (val === "=") {
+          calculate();
+        } else {
+          setInputs(inputs + val);
+        }
       }
     }
   };
